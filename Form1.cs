514dcf133d3f6a234ce33a5b09ff247db9a84b00
Кожев Андрей <103951737+AndreyKozhev@users.noreply.github.com>
2022-04-22 16:18:50 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsRun
+{
+    
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr SendMessage(HandleRef hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(comboBox1.Text);
+            }
+            catch
+            {
+                Application.Exit();
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(comboBox1.Text);
+                }
+                catch
+                {
+                    Application.Exit();
+                }
+                finally
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.FlatStyle = FlatStyle.System;
+            SendMessage(new HandleRef(button1, button1.Handle), 0x160C, IntPtr.Zero, true ? new IntPtr(1) : IntPtr.Zero);
+        }
+    }
+}
